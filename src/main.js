f1ca4d47d5f7bed35c95d1d9e7f6b5ec6a2965f1
Guillermo Howard <129241417+GuillermoPromac@users.noreply.github.com>
2025-10-10 06:32:30 -0500

    feats: resolve  merge

--- src/main.js
+++ src/main.js
@@ -1,10 +1,4 @@
 import { createApp } from 'vue'
-<<<<<<< HEAD
-import './style.css'
-import App from './App.vue'
-
-createApp(App).mount('#app')
-=======
 import { createPinia } from 'pinia'
 import router from './router'
 import App from './App.vue'
@@ -14,4 +8,3 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
->>>>>>> signlingo-vue
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-<<<<<<< HEAD
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
-=======
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -15,4 +9,3 @@ export default defineConfig({
     },
   },
 })
->>>>>>> signlingo-vue
